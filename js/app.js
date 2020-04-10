643f1b6593305df576b2d2f0c3efbdfc9acd6b3c
Izzad <18FTT1711@student.pb.edu.bn>
2020-04-10 22:26:57 +0800

    nineteen commit

--- js/app.js
+++ js/app.js
@@ -1,9 +1,8 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/serviceWorker.js', {scope: "https://izzadsalleh.github.io/IBsBlog/"}).then(function(registration) {
-      console.log('Service worker registration succeeded:', registration);
-    }, function(error) {
-      console.log('Service worker registration failed:', error);
-    });
-  } else {
-    console.log('Service workers are not supported.');
+window.onload = () => {
+  'use strict';
+
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('./sw.js');
   }
+}
