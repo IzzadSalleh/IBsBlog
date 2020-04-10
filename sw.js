e614a39b70350626810964fc8fc516e91c4f50b1
Izzad <18FTT1711@student.pb.edu.bn>
2020-04-10 23:05:07 +0800

    twenty commit

--- sw.js
+++ sw.js
@@ -1,103 +1,58 @@
-// const staticIBblog = "ib-blog-site-v1";
+var APP_PREFIX = 'IBsBlog'     // Identifier for this app (this needs to be consistent across every cache update)
+var VERSION = 'version_01'              // Version of the off-line cache (change this value everytime you want to update cache)
+var CACHE_NAME = APP_PREFIX + VERSION
+var URLS = [                            // Add URL you want to cache in this list.
+  '/{repository}/',                     // If you have separate JS/CSS files,
+  '/{repository}/index.html'            // add path to those files here
+]
 
-// const assets = [
-//     "/",
-//     "/index.html",
-//     "/post.html",
-//     "/about.html",
-//     "/vendor/bootstrap/css/bootstrap.min.css",
-//     "/vendor/fontawesome-free/css/all.min.css",
-//     "https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic",
-//     "https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800",
-//     "/css/clean-blog.min.css",
-//     "/vendor/bootstrap/js/bootstrap.bundle.min.js",
-//     "/js/clean-blog.min.js",
-//     "/img/home-bg.png",
-//     "/img/post-bg.png",
-//     "/img/about-bg.png",
-//     "/img/hvita.png",
-//     "/img/oxara.png",
-//     "/img/skoga.png",
-//   ];
+// Respond with cached resources
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request : ' + e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) { // if cache is available, respond with cache
+        console.log('responding with cache : ' + e.request.url)
+        return request
+      } else {       // if there are no cache, try fetching request
+        console.log('file is not cached, fetching : ' + e.request.url)
+        return fetch(e.request)
+      }
 
+      // You can omit if/else for console.log & put one line below like this too.
+      // return request || fetch(e.request)
+    })
+  )
+})
 
-// self.addEventListener("install", installEvent => {
-//     installEvent.waitUntil(
-//       caches.open(staticIBblog).then(cache => {
-//         cache.addAll(assets)
-//       })
-//     )
-//   });
+// Cache resources
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('installing cache : ' + CACHE_NAME)
+      return cache.addAll(URLS)
+    })
+  )
+})
 
-//   self.addEventListener("fetch", fetchEvent => {
-//     fetchEvent.respondWith(
-//       caches.match(fetchEvent.request).then(res => {
-//         return res || fetch(fetchEvent.request)
-//       })
-//     )
-//   });
-
-  var cacheName = 'hello-pwa';
-  var filesToCache = [
-    "/",
-    "/index.html",
-    "/post.html",
-    "/about.html",
-    "/vendor/bootstrap/css/bootstrap.min.css",
-    "/vendor/fontawesome-free/css/all.min.css",
-    "https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic",
-    "https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800",
-    "/css/clean-blog.min.css",
-    "/vendor/bootstrap/js/bootstrap.bundle.min.js",
-    "/js/clean-blog.min.js",
-    "/img/home-bg.png",
-    "/img/post-bg.png",
-    "/img/about-bg.png",
-    "/img/hvita.png",
-    "/img/oxara.png",
-    "/img/skoga.png",
-  ];
-
-/* Start the service worker and cache all of the app's content */
-// self.addEventListener('install', function(e) {
-//   e.waitUntil(
-//     caches.open(cacheName).then(function(cache) {
-//       return cache.addAll(filesToCache);
-//     })
-//   );
-// });
-
-//  Serve cached content when offline 
-// self.addEventListener('fetch', function(e) {
-//   e.respondWith(
-//     caches.match(e.request).then(function(response) {
-//       return response || fetch(e.request);
-//     })
-//   );
-// });
-
-addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response;     // if valid response is found in cache return it
-        } else {
-          return fetch(event.request)     //fetch from internet
-            .then(function(res) {
-              return caches.open(CACHE_DYNAMIC_NAME)
-                .then(function(cache) {
-                  cache.put(event.request.url, res.clone());    //save the response for future
-                  return res;   // return the fetched data
-                })
-            })
-            .catch(function(err) {       // fallback mechanism
-              return caches.open(CACHE_CONTAINING_ERROR_MESSAGES)
-                .then(function(cache) {
-                  return cache.match('/offline.html');
-                });
-            });
-        }
+// Delete outdated caches
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      // `keyList` contains all cache names under your username.github.io
+      // filter out ones that has this app prefix to create white list
+      var cacheWhitelist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX)
       })
-  );
-}); 
+      // add current cache name to white list
+      cacheWhitelist.push(CACHE_NAME)
+
+      return Promise.all(keyList.map(function (key, i) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          console.log('deleting cache : ' + keyList[i] )
+          return caches.delete(keyList[i])
+        }
+      }))
+    })
+  )
+})
