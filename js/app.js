deaaaa20bbb6a7363309a2f6c6bcc6ec089cc0b9
Izzad <18FTT1711@student.pb.edu.bn>
2020-04-10 21:57:59 +0800

    fifteen commit

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/serviceWorker.js', {scope: './'}).then(function(registration) {
+    navigator.serviceWorker.register('/serviceWorker.js', {scope: "https://izzadsalleh.github.io/IBsBlog/"}).then(function(registration) {
       console.log('Service worker registration succeeded:', registration);
     }, function(error) {
       console.log('Service worker registration failed:', error);
